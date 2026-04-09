e829a34878e4be705564b3e698a5302701184d9f
nolanflores <nolan.t.flores@gmail.com>
2026-04-08 21:22:19 -0700

    Simple start on verilog logic

---
+++ quartus/Control_Unit.sv
@@ -0,0 +1,12 @@
+module ControlUnit(
+	input logic clock,
+	input logic reset,
+	input logic [7:0] instruction,
+	
+	output logic instruction_reg_en,
+	output logic address_reg_en,
+	output logic pc_en,
+	output logic ac_en
+);
+
+endmodule
---
+++ quartus/Register.sv
@@ -0,0 +1,19 @@
+module Register(
+	input logic clock,
+	input logic enable,
+	input logic reset_n,
+	input logic [7:0] in,
+	output logic [7:0] out
+);
+
+always_ff @(posedge clock, negedge reset_n) begin
+	if(!reset_n) begin
+		out <= 0;
+	end else begin
+		if(enable) begin
+			out <= in;
+		end
+	end
+end
+
+endmodule
